ca881bc59d48fac1291ffbb74e23696d60438b55
shubham1995kumar <shubhamlmar30@gmail.com>
2023-01-28 00:46:35 +0530

    adding crouch and Jump control

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   
     public Animator animator;
     private void Awake()
     {
@@ -16,21 +17,52 @@ public class PlayerController : MonoBehaviour
     //}
     private void Update()
     {
+        float jump = Input.GetAxisRaw("Vertical");
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
 
         Vector3 scale = transform.localScale;
         if (speed < 0)
         {
-            
+
             scale.x = -1f * Mathf.Abs(scale.x);
 
-        }else if (speed > 0)
+        }
+        else if (speed > 0)
         {
 
             scale.x = Mathf.Abs(scale.x);
         }
 
         transform.localScale = scale;
+        if (jump > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+        }
+
+
+
     }
+
+
+
+   
+
+
+
+
 }

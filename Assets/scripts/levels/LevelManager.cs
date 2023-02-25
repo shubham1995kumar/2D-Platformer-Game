4d9457759b5aaf1eb2c68293a8b73b2951c63e2c
shubham1995kumar <shubhamlmar30@gmail.com>
2023-02-25 23:37:50 +0530

    Singleton Implementation

---
+++ Assets/scripts/levels/LevelManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public string Level1;
+    public string[] Levels;
+    private static LevelManager instance;
+    public static LevelManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        SetLevelStatus(Levels[0], LevelStatus.Unlocked); // unlock Level 1
+        for (int i = 1; i < Levels.Length; i++)
+        {
+            SetLevelStatus(Levels[i], LevelStatus.Locked); // lock all other levels
+        }
+    }
+
+    public void MarkCurrentLevelComplete()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SetLevelStatus(currentScene.name, LevelStatus.Completed);
+
+        int currentSceneIndex = Array.FindIndex(Levels, Levels => Levels == currentScene.name);
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex < Levels.Length)
+        {
+            SetLevelStatus(Levels[nextSceneIndex], LevelStatus.Unlocked);
+        }
+    }
+
+    public LevelStatus GetLevelStatus(string level)
+    {
+        LevelStatus levelStatus = (LevelStatus)PlayerPrefs.GetInt(level, 0);
+        return levelStatus;
+    }
+
+    public void SetLevelStatus(string level, LevelStatus levelStatus)
+    {
+        PlayerPrefs.SetInt(level, (int)levelStatus);
+        Debug.Log("Setting Level: " + level + " status: " + levelStatus);
+    }
+}
+
+
